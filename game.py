decdca43381d34dae8ab35925000d2343a78e4bc
JasserRech <jahrech@gmail.com>
2023-09-13 18:10:16 +0100

    feat: create deck and function to shuffle cards

--- game.py
+++ game.py
@@ -1,2 +1,20 @@
-print('Hello world')
-print('test')
+import random
+from time import sleep
+
+suits_values = {
+    'J': 10,
+    'Q': 10,
+    'K': 10,
+    'A': 11,
+}
+
+# Criar deck
+def new_deck():
+    suits = ['\u2663', '\u2666', '\u2665', '\u2660']
+    ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    deck = []
+    for suit in suits:
+        for value in ranks:
+            deck.append(f'{value} {suit}')
+    random.shuffle(deck)
+    return deck
