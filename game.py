0fcbf248b6d5b420acf3bf5ed2aec513519b1e70
JasserRech <jahrech@gmail.com>
2023-09-13 18:36:35 +0100

    feat: add function to print player and dealer hands

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ suits_values = {
     'A': 11,
 }
 
-# Criar deck
+# function to create and shuffle deck
 def new_deck():
     suits = ['\u2663', '\u2666', '\u2665', '\u2660']
     ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -18,3 +18,12 @@ def new_deck():
             deck.append(f'{value} {suit}')
     random.shuffle(deck)
     return deck
+
+# function to print player and dealer hand
+def show_result(player_hand, dealer_hand):
+    print('Player hand:')
+    print(player_hand)
+    print('Dealer hand:')
+    print(dealer_hand)
+
+    
