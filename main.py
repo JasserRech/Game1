18a73ee5dc7eb5c05de82cd910998da83a8bee22
JasserRech <jahrech@gmail.com>
2023-09-18 20:07:29 +0100

    feat: add welcome message to initial screen

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import game
+import pyfiglet
 from clear import clear
 
+
+
 def initial_screen():
+    font = pyfiglet.Figlet()
+    message = "Welcome to Blackjack"
+    rendered_message = font.renderText(message)
+    print(rendered_message)
     while True:
         play_game = str(input('Do you want to play a game? (y/n): ')) 
         if play_game in 'yYnN':
             return play_game
-
 
 
 while True:
